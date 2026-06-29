d467f8e55d94cf8a000882fb4c2e0a8ab304c5a0
hardik211005 <hardikdhameeja2105@gmail.com>
2026-06-29 16:28:23 +0530

    Update CORS configuration to allow multiple origins

--- server/index.js
+++ server/index.js
@@ -17,7 +17,9 @@ const port=process.env.PORT || 3000
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin: ["http://localhost:5173",
+    "https://genwebai-nu.vercel.app"
+    ],
     credentials:true
 }))
 app.use("/api/auth",authRouter)
