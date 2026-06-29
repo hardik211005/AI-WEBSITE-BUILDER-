bb90fe63f0b9b1b77ec3318d1f34570cfb702c64
hardik211005 <hardikdhameeja2105@gmail.com>
2026-06-29 16:53:10 +0530

    Update cookie settings in googleAuth to enhance security

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -14,12 +14,12 @@ try {
     }
     const token=await jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"7d"})
 
-    res.cookie("token",token,{
-        httpOnly:true,
-        secure:false,
-        sameSite:"strict",
-        maxAge:7*24*60*60*1000
-    })
+    res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,           
+  sameSite: "none",       
+  maxAge: 7 * 24 * 60 * 60 * 1000
+});
 
     return res.status(200).json(user)
 } catch (error) {
